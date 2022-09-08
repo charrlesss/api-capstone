6ec6044087b99bcac4f98654fcdd35ade554802e
chinanumbawanph <charlespalencia21@gmail.com>
2022-09-08 21:43:55 +0800

    f

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const connectDB = require("./connect.db.js");
 const getAllProducts = require("./model/methods/get-all-products.js");
+const cors = require('cors')
 const app = express();
 const PORT = 4000;
 
@@ -11,6 +12,7 @@ async function main() {
   try {
 
     app.use(express.json())
+    app.use(cors({origin:"http://localhost:3000" , credentials:true}))
 
     app.get("/",async (req, res) => {
 
