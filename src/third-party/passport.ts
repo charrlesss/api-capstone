0f9e642c63dc1c6c3f735a1c024c7044488162fa
chinanumbawanph <charlespalencia21@gmail.com>
2022-10-16 11:37:54 +0800

    f

--- src/third-party/passport.ts
+++ src/third-party/passport.ts
@@ -52,7 +52,7 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_APP_ID as string,
       clientSecret: process.env.FACEBOOK_APP_SECRET as string,
-      callbackURL: "/facebook/callback",
+      callbackURL: process.env.NODE_ENV === 'dev' ?"/facebook/callback" : "https://api-admin-capstone.herokuapp.com/facebook/callback",
       profileFields: [
         "id",
         "displayName",
@@ -62,6 +62,7 @@ passport.use(
         "email",
         "link",
       ],
+      
     },
     async function (accessToken, refreshToken, profile: any, done) {
       const data = {
